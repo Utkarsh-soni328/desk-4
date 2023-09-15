0c0d8d67eea01b8ff4c365b566e73eb44b80e35a
Utkarsh-soni328 <124017589+Utkarsh-soni328@users.noreply.github.com>
2023-09-15 12:51:32 +0530

    Add files via upload

---
+++ src/pages/desktop4.js
@@ -0,0 +1,182 @@
+const Desktop4 = () => {
+  return (
+    <div className="relative bg-white w-full h-[1929px] overflow-hidden text-left text-base text-black font-inter">
+      <div className="absolute top-[0px] left-[0px] w-[1440px] h-[220px] text-center">
+        <div className="absolute top-[0px] left-[0px] bg-royalblue w-[1440px] h-[220px]" />
+        <div className="absolute top-[165px] left-[1289px] inline-block w-[141px] h-8">
+          Login/signup
+        </div>
+        <div className="absolute top-[77px] left-[43px] bg-gainsboro w-[291px] h-10" />
+        <div className="absolute top-[91px] left-[408px] w-[866px] h-[33px] text-xl">
+          <b className="absolute top-[6.29px] left-[584.99px] inline-block w-[143.24px] h-[26.71px]">
+            Financial Aids
+          </b>
+          <b className="absolute top-[3.14px] left-[358.65px] inline-block w-[103.88px] h-[26.71px]">
+            Lab
+          </b>
+          <b className="absolute top-[0px] left-[211.03px] inline-block w-[154.17px] h-[29.86px]">
+            Get Resources
+          </b>
+          <b className="absolute top-[0px] left-[107.16px] inline-block w-[103.88px] h-[26.71px]">
+            Learn
+          </b>
+          <b className="absolute top-[0px] left-[0px] inline-block w-[103.88px] h-[26.71px]">
+            About Us
+          </b>
+          <b className="absolute top-[3.14px] left-[450.49px] inline-block w-[117px] h-[26.71px]">
+            Job Portal
+          </b>
+          <b className="absolute top-[6.29px] left-[747.91px] inline-block w-[118.09px] h-[26.71px]">
+            Contact Us
+          </b>
+        </div>
+        <img
+          className="absolute h-[52.27%] w-[8.06%] top-[18.18%] right-[1.59%] bottom-[29.55%] left-[90.35%] max-w-full overflow-hidden max-h-full"
+          alt=""
+          src="/group.svg"
+        />
+      </div>
+      <div className="absolute top-[1462px] left-[0px] w-[1440px] h-[467px] text-3xl text-white">
+        <div className="absolute top-[0px] left-[0px] bg-royalblue w-[1440px] h-[467px]" />
+        <img
+          className="absolute top-[360px] left-[0px] w-[1440px] h-[107px]"
+          alt=""
+          src="/rectangle-128.svg"
+        />
+        <div className="absolute top-[42px] left-[826px] w-[452px] h-[307px]">
+          <div className="absolute top-[50px] left-[35px] inline-block w-[417px] h-[104px]">
+            For any queries regarding the NPTEL website,availability of courses
+            or issues in accessing courses,please contact
+          </div>
+          <div className="absolute top-[0px] left-[0px] text-5xl text-center inline-block w-[194px] h-[38px]">
+            Contact Us:
+          </div>
+          <div className="absolute top-[168px] left-[28px] text-[inherit] inline-block w-[415px] h-[139px]">
+            {`NPTEL Administrator,IC & SR, 3rd floorIIT Madras, Chennai - 600036Tel : `}
+            <a
+              className="text-[inherit]"
+              href="tel:044 2257 5905"
+              target="_blank"
+            >
+              <span className="[text-decoration:underline]">
+                (044) 2257 5905
+              </span>
+            </a>
+            {`, `}
+            <a
+              className="text-[inherit]"
+              href="tel:044 2257 5908"
+              target="_blank"
+            >
+              <span className="[text-decoration:underline]">
+                (044) 2257 5908
+              </span>
+            </a>
+            {`, `}
+            <a className="text-[inherit]" href="tel:9363218521" target="_blank">
+              <span className="[text-decoration:underline]">9363218521</span>
+            </a>
+            {` (Mon-Fri 9am-6pm)Email : `}
+            <a
+              className="text-[inherit]"
+              href="mailto:support@nptel.iitm.ac.in"
+              target="_blank"
+            >
+              <span className="[text-decoration:underline]">
+                support@nptel.iitm.ac.in
+              </span>
+            </a>
+          </div>
+        </div>
+        <div className="absolute top-[42px] left-[162px] w-[194px] h-64 text-5xl">
+          <div className="absolute top-[0px] left-[0px] inline-block w-[194px] h-[38px]">
+            Quick Links
+          </div>
+          <div className="absolute top-[218px] left-[0px] inline-block w-[194px] h-[38px]">
+            Quick Links
+          </div>
+          <div className="absolute top-[173px] left-[0px] inline-block w-[194px] h-[38px]">
+            Quick Links
+          </div>
+          <div className="absolute top-[128px] left-[0px] inline-block w-[194px] h-[38px]">
+            Quick Links
+          </div>
+          <div className="absolute top-[83px] left-[0px] inline-block w-[194px] h-[38px]">
+            Quick Links
+          </div>
+        </div>
+        <b className="absolute top-[401px] left-[140px] text-xl inline-block text-center w-[317px] h-[25px]">
+          © - 2022 All rights reserved
+        </b>
+      </div>
+      <div className="absolute top-[421px] left-[0px] w-[1440px] h-[1041px] text-21xl text-white">
+        <div className="absolute top-[0px] left-[0px] bg-lightskyblue w-[1440px] h-[1041px]" />
+        <div className="absolute top-[520px] left-[411px] rounded-25xl bg-lightsteelblue w-[618px] h-[88px]" />
+        <div className="absolute top-[27px] left-[411px] w-[618px] h-[831px]">
+          <div className="absolute top-[0px] left-[0px] w-[618px] h-[831px]">
+            <div className="absolute top-[0px] left-[0px] rounded-[40px] bg-darkslateblue-100 w-[618px] h-[456px]" />
+            <div className="absolute top-[743px] left-[0px] rounded-25xl bg-darkslateblue-100 w-[618px] h-[88px]" />
+            <div className="absolute top-[618px] left-[0px] rounded-25xl bg-darkslateblue-100 w-[618px] h-[88px]" />
+            <div className="absolute top-[493px] left-[0px] rounded-25xl bg-darkslateblue-100 w-[618px] h-[88px]" />
+          </div>
+          <div className="absolute top-[11px] left-[45px] w-[293px] h-[792px]">
+            <div className="absolute top-[0px] left-[0px] text-black inline-block w-[291px] h-[45px]">
+              Physics
+            </div>
+            <div className="absolute top-[747px] left-[2px] inline-block w-[291px] h-[45px]">
+              Bio
+            </div>
+            <div className="absolute top-[626px] left-[1px] inline-block w-[291px] h-[45px]">
+              Maths
+            </div>
+            <div className="absolute top-[501px] left-[0px] inline-block w-[291px] h-[45px]">
+              Chemistry
+            </div>
+          </div>
+          <img
+            className="absolute top-[512px] left-[543px] w-[55px] h-[300px]"
+            alt=""
+            src="/triangle.svg"
+          />
+        </div>
+        <div className="absolute top-[52px] left-[456px] w-[585px] h-[388px]">
+          <a href="https://strong-meerkat-61be88.netlify.app/" className="text-white">
+          <div className="absolute top-[116px] left-[0px] inline-block w-[217px] h-11">
+            Kinematics
+          </div>
+          </a>
+          <div className="absolute top-[344px] left-[0px] inline-block w-[217px] h-11">
+            Another
+          </div>
+          <div className="absolute top-[287px] left-[0px] inline-block w-[217px] h-11">
+            3-D
+          </div>
+          <div className="absolute top-[230px] left-[0px] inline-block w-[217px] h-11">
+            Projectile
+          </div>
+          <div className="absolute top-[173px] left-[0px] inline-block w-[217px] h-11">
+            Rotational
+          </div>
+          <div className="absolute top-[0px] left-[486px] text-13xl text-black inline-block w-[99px] h-[47px]">
+            4/10
+          </div>
+        </div>
+        <div className="absolute top-[200px] left-[970px] w-[22px] h-[164px]">
+          <div className="absolute top-[0px] left-[0px] bg-white w-[22px] h-[164px]" />
+          <div className="absolute top-[7px] left-[3px] bg-tomato w-4 h-5" />
+        </div>
+      </div>
+      <div className="absolute top-[256px] left-[42px] w-[1130px] h-[146px] text-5xl">
+        <div className="absolute top-[66px] left-[51px] inline-block w-[1079px] h-20">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque
+          iaculis mi lorem. Praesent congue quis libero a egestas. Aenean ornar
+        </div>
+        <div className="absolute top-[0px] left-[0px] text-29xl text-center inline-block w-[293px] h-[66px]">
+          Class-12
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Desktop4;
